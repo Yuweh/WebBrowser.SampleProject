7b3ddd3ad3202e02053b5d3b0b1746203b35d8fb
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-13 22:15:00 +0800

    Default Project set@Google.com

--- WebBrowser.SampleProject/ViewController.swift
+++ WebBrowser.SampleProject/ViewController.swift
@@ -8,11 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate {
 
+    
+    @IBOutlet weak var mySearchBar: UISearchBar!
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    
+    @IBAction func back(_ sender: Any) {
+        if myWebView.canGoBack
+        {
+            myWebView.goBack()
+        }
+    }
+    
+    
+    @IBAction func next(_ sender: Any) {
+        if myWebView.canGoForward
+        {
+            myWebView.goForward()
+        }
+    }
+    
+    
+    @IBAction func refresh(_ sender: Any) {
+        myWebView.reload()
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        mySearchBar.resignFirstResponder()
+        
+        if let url = URL(string: mySearchBar.text!)
+        {
+            myWebView.loadRequest(URLRequest(url: url))
+        }
+        else
+        {
+            print ("ERROR")
+        }
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myWebView.loadRequest(URLRequest(url: URL(string: "https://www.google.com")!))
     }
 
     override func didReceiveMemoryWarning() {
